4c9bd0d9242cc754275e8214f6b3d3048a36257e
Jung Gyu Yoon <yjg30737@gmail.com>
2022-02-05 08:19:45 +0900

    Add feature(switch moving animation), update version (0.0.1 -> 0.1.0)

--- pyqt_switch/pyqtSwitch.py
+++ pyqt_switch/pyqtSwitch.py
@@ -1,4 +1,4 @@
-from PyQt5.QtCore import Qt, pyqtSignal
+from PyQt5.QtCore import Qt, pyqtSignal, QPropertyAnimation, QPoint, QAbstractAnimation
 
 from PyQt5.QtWidgets import QWidget, QPushButton, QGridLayout, QHBoxLayout
 
@@ -6,10 +6,11 @@ from PyQt5.QtWidgets import QWidget, QPushButton, QGridLayout, QHBoxLayout
 class PyQtSwitch(QWidget):
     toggled = pyqtSignal(bool)
 
-    def __init__(self):
+    def __init__(self, animation_enabled_flag=False):
         super().__init__()
         self.__btn_width = 20
         self.__btn_height = 20
+        self.__animationEnabledFlag = animation_enabled_flag
         self.__initUi()
 
     def __initUi(self):
@@ -35,15 +36,30 @@ class PyQtSwitch(QWidget):
         self.setLayout(lay)
         self.setFixedSize(self.__btn_width * 2, self.__btn_height)
 
+        if self.__animationEnabledFlag:
+            self.__animation = QPropertyAnimation(self, b'point')
+            self.__animation.valueChanged.connect(self.__btn.move)
+            self.__animation.setDuration(100)
+            self.__animation.setStartValue(QPoint(0, 0))
+            self.__animation.setEndValue(QPoint(self.__btn_width, 0))
+
     def mousePressEvent(self, e):
         self.__btn.toggle()
         return super().mousePressEvent(e)
 
     def __toggled(self, f):
-        if f:
-            self.__btn.move(self.__btn_width, 0)
-            self.__layForBtnAlign.setAlignment(Qt.AlignRight)
+        if self.__animationEnabledFlag:
+            if f:
+                self.__animation.setDirection(QAbstractAnimation.Forward)
+                self.__animation.start()
+            else:
+                self.__animation.setDirection(QAbstractAnimation.Backward)
+                self.__animation.start()
         else:
-            self.__btn.move(0, 0)
-            self.__layForBtnAlign.setAlignment(Qt.AlignLeft)
+            if f:
+                self.__btn.move(self.__btn_width, 0)
+                self.__layForBtnAlign.setAlignment(Qt.AlignRight)
+            else:
+                self.__btn.move(0, 0)
+                self.__layForBtnAlign.setAlignment(Qt.AlignLeft)
         self.toggled.emit(f)

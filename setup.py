3546cb9c595e166a5937a23ef96c6a8242be6cf4
Jung Gyu Yoon <yjg30737@gmail.com>
2022-05-07 18:46:49 +0900

    Update setup.py for PyPi description, bump version (0.0.1 -> 0.0.11)

--- setup.py
+++ setup.py
@@ -1,14 +1,24 @@
+import codecs
+import os
+
 from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
 
 setup(
     name='pyqt-switch',
-    version='0.0.1',
+    version='0.0.11',
     author='Jung Gyu Yoon',
     author_email='yjg30737@gmail.com',
     license='MIT',
     packages=find_packages(),
     description='PyQt Switch (O ) -> ( O)',
     url='https://github.com/yjg30737/pyqt-switch.git',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
     install_requires=[
         'PyQt5>=5.8'
     ]

038db7b130da383bbc54f007090bd58fc827005f
Jung Gyu Yoon <yjg30737@gmail.com>
2022-05-18 17:34:40 +0900

    Add color transition animation

--- pyqt_switch/pyqtSwitch.py
+++ pyqt_switch/pyqtSwitch.py
@@ -6,18 +6,22 @@ from PyQt5.QtWidgets import QWidget, QPushButton, QGridLayout, QHBoxLayout
 class PyQtSwitch(QWidget):
     toggled = pyqtSignal(bool)
 
-    def __init__(self, animation_enabled_flag=False):
+    def __init__(self):
         super().__init__()
+        self.__initVal()
+        self.__initUi()
+
+    def __initVal(self):
         self.__btn_width = 20
         self.__btn_height = 20
-        self.__animationEnabledFlag = animation_enabled_flag
-        self.__initUi()
+        self.__animationEnabledFlag = False
+        self.__pointAnimation = ''
+        self.__colorAnimation = ''
 
     def __initUi(self):
         self.__btn = QPushButton()
         self.__btn.setCheckable(True)
         self.__btn.setFixedSize(self.__btn_width, self.__btn_height)
-        self.__btn.setStyleSheet('QPushButton { background-color: #FFF; }')
         self.__btn.toggled.connect(self.__toggled)
 
         self.__layForBtnAlign = QHBoxLayout()
@@ -27,7 +31,7 @@ class PyQtSwitch(QWidget):
 
         innerWidgetForStyle = QWidget()
         innerWidgetForStyle.setLayout(self.__layForBtnAlign)
-        self.setStyleSheet(f'QWidget {{ border: 1px solid #AAA; border-radius: {self.__btn_height // 2}px; }}')
+        self.setStyleSheet(f'QWidget {{ border: 1px solid #AAAAAA; border-radius: {self.__btn_height // 2}px; }}')
 
         lay = QGridLayout()
         lay.addWidget(innerWidgetForStyle)
@@ -36,12 +40,20 @@ class PyQtSwitch(QWidget):
         self.setLayout(lay)
         self.setFixedSize(self.__btn_width * 2, self.__btn_height)
 
+    def setAnimation(self, f: bool):
+        self.__animationEnabledFlag = f
         if self.__animationEnabledFlag:
-            self.__animation = QPropertyAnimation(self, b'point')
-            self.__animation.valueChanged.connect(self.__btn.move)
-            self.__animation.setDuration(100)
-            self.__animation.setStartValue(QPoint(0, 0))
-            self.__animation.setEndValue(QPoint(self.__btn_width, 0))
+            self.__colorAnimation = QPropertyAnimation(self, b'point')
+            self.__colorAnimation.valueChanged.connect(self.__btn.move)
+            self.__colorAnimation.setDuration(100)
+            self.__colorAnimation.setStartValue(QPoint(0, 0))
+            self.__colorAnimation.setEndValue(QPoint(self.__btn_width, 0))
+
+            self.__pointAnimation = QPropertyAnimation(self, b'color')
+            self.__pointAnimation.valueChanged.connect(self.__styleInit)
+            self.__pointAnimation.setDuration(100)
+            self.__pointAnimation.setStartValue(255)
+            self.__pointAnimation.setEndValue(200)
 
     def mousePressEvent(self, e):
         self.__btn.toggle()
@@ -50,16 +62,25 @@ class PyQtSwitch(QWidget):
     def __toggled(self, f):
         if self.__animationEnabledFlag:
             if f:
-                self.__animation.setDirection(QAbstractAnimation.Forward)
-                self.__animation.start()
+                self.__colorAnimation.setDirection(QAbstractAnimation.Forward)
+                self.__colorAnimation.start()
+                self.__pointAnimation.setDirection(QAbstractAnimation.Forward)
+                self.__pointAnimation.start()
             else:
-                self.__animation.setDirection(QAbstractAnimation.Backward)
-                self.__animation.start()
+                self.__colorAnimation.setDirection(QAbstractAnimation.Backward)
+                self.__colorAnimation.start()
+                self.__pointAnimation.setDirection(QAbstractAnimation.Backward)
+                self.__pointAnimation.start()
         else:
             if f:
                 self.__btn.move(self.__btn_width, 0)
                 self.__layForBtnAlign.setAlignment(Qt.AlignRight)
+                self.__styleInit(200)
             else:
                 self.__btn.move(0, 0)
                 self.__layForBtnAlign.setAlignment(Qt.AlignLeft)
+                self.__styleInit(255)
         self.toggled.emit(f)
+
+    def __styleInit(self, f: int):
+        self.__btn.setStyleSheet(f'QPushButton {{ background-color: rgb({f}, {f}, 255); }}')
